1956a1aa0a7066ac3c9fee5ebe26c12950279686
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-11-11 23:01:21 +0000

    proven that code can fetch a package

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,46 @@
-import * as esbuild from 'esbuild-wasm'
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startServer = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: '/esbuild.wasm',
     });
   };
+  useEffect(() => {
+    startServer();
+  }, []);
 
-  const onClick: any = () => {
-    console.log(input);
+  const onClick:any = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    });
+   
+    //console.log(result);
+
+    setCode(result.outputFiles[0].text);
+
   };
 
   return(
     <div>
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
-        <button onClick={onClick()}>Submit</button>
+        <button onClick={() => onClick()}>Submit</button>
       </div>
       <pre>{code}</pre>
     </div>
