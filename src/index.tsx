e58464772988958228138435629097dde95a8c8c
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-11-12 23:19:17 +0000

    bundling code inside the browser and rendering it in iframe, and directly importing a package from unpkg.com

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,14 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startServer = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -38,24 +39,35 @@ const App = () => {
       },
     });
    
-    //console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    //setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
 
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return(
     <div>
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
-        <button onClick={() => onClick()}>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
     </div>
   );
 };
 
-ReactDOM.render(
-  <App/>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App/>, document.querySelector('#root'));
