29daf4db615568f0aae25b963ca02ae05dfca6da
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-11-12 00:11:09 +0000

    improving unresolve function and generating a dynamic(nested) path

--- src/plugins/unpkg-path-plugins.ts
+++ src/plugins/unpkg-path-plugins.ts
@@ -10,11 +10,22 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if(args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', 
+        } 
+
+        if (args.path.includes('./') || args.path.includes('../')){
+          return {
             namespace: 'a',
+            path: new URL(
+              args.path, 'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
           };
         }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        }
+
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -24,17 +35,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         } 
         
 
-        const { data } = await axios.get(args.path);
+        const { data,request } = await axios.get(args.path);
+        console.log(request);
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
       });
     },
