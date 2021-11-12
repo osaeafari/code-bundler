74edfe554eeff6d2278b02804464e4c5dbf22673
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-11-12 10:11:35 +0000

    code refactoring, taking input from textarea

--- src/plugins/unpkg-path-plugins.ts
+++ src/plugins/unpkg-path-plugins.ts
@@ -8,7 +8,7 @@ const fileCache = localforage.createInstance({
 
 
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -40,10 +40,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, {useState} from 'react';
-              console.log(React, useState);
-            `,
+            contents: inputCode,
           };
         } 
         
