aaed5a0ebdd44f9c4133b81985e0124b75badd6d
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-11-15 10:00:49 +0000

    added horizontal reziable component to code cell

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import React, { useEffect, useRef} from "react";
 
 interface PreviewProps {
@@ -33,12 +34,15 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-      title="preview" 
-      ref={iframe} 
-      sandbox="allow-scripts" 
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview" 
+        style={{ backgroundColor: 'white' }}
+        ref={iframe} 
+        sandbox="allow-scripts" 
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
---
+++ src/components/resizable.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './resizable.css'; 
+import { ResizableBox } from "react-resizable";
+
+interface ResizableProps {
+  direction: 'horizontal' | 'vertical';
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  return(
+    <ResizableBox 
+      minConstraints={[Infinity, 24]}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]} 
+      height= {300} 
+      width={Infinity} 
+      resizeHandles={['s']}
+    >
+      {children}
+    </ResizableBox>
+  ); 
+};
+
+export default Resizable
