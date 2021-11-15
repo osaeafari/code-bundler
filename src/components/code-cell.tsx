d882d7ffbb666534454c5dd40093b0c20f4b4ff6
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-11-15 11:34:44 +0000

    added vertical reziable component to code cell

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,10 +16,12 @@ const CodeCell = () => {
   return(
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor 
-          initialValue="console.log(123);"
-          onChange={(value) => setInput(value)}  
-        />
+        <Resizable direction='horizontal'>
+          <CodeEditor 
+            initialValue="console.log(123)"
+            onChange={(value) => setInput(value)}  
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
